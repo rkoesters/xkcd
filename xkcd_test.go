153a3e25afa95c37f22b7e1bfa67d6b39640716b
Ryan Koesters <rmkoesters@gmail.com>
2013-10-06 22:53:43 -0500

    Added tests.

---
+++ xkcd_test.go
@@ -0,0 +1,52 @@
+package xkcd_test
+
+import (
+	"github.com/rkoesters/xkcd"
+	"testing"
+)
+
+func TestGet(t *testing.T) {
+	comic, err := xkcd.Get(221)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	expected := &xkcd.Comic{
+		Num:       221,
+		Title:     "Random Number",
+		SafeTitle: "Random Number",
+		Img:       "http://imgs.xkcd.com/comics/random_number.png",
+		Alt:       "RFC 1149.5 specifies 4 as the standard IEEE-vetted random number.",
+		Year:      "2007",
+		Month:     "2",
+		Day:       "9",
+		News:      "",
+		Link:      "",
+		Transcript: `int getRandomNumber()
+{
+  return 4; 
+ chosen by fair dice roll.
+               
+ guarenteed to be random.
+}
+{{title text: RFC 1149.5 specifies 4 as the standard IEEE-vetted random number.}}`,
+	}
+
+	if *comic != *expected {
+		t.Fail()
+	}
+}
+
+func TestGetCurrent(t *testing.T) {
+	_, err := xkcd.GetCurrent()
+	if err != nil {
+		t.Fatal(err)
+	}
+}
+
+func Test404(t *testing.T) {
+	_, err := xkcd.Get(404)
+	if err.Error() != "404 Not Found" {
+		t.Fatal(err)
+	}
+}
