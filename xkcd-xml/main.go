34bcd85097af82191561e9a1100feeac287ef631
Ryan Koesters <rmkoesters@gmail.com>
2013-10-21 16:48:49 -0500

    Added example xkcd-xml.

---
+++ xkcd-xml/main.go
@@ -0,0 +1,45 @@
+/*
+xkcd-xml prints information about a xkcd comic in xml format.
+
+Usage:
+	xkcd-xml [-n number]
+*/
+package main
+
+import (
+	"encoding/xml"
+	"flag"
+	"github.com/rkoesters/xkcd"
+	"log"
+	"os"
+)
+
+var number = flag.Int("n", 0, "Comic number.")
+
+func main() {
+	flag.Parse()
+
+	if flag.NArg() != 0 {
+		flag.Usage()
+		os.Exit(1)
+	}
+
+	var comic *xkcd.Comic
+	var err error
+
+	if *number == 0 {
+		comic, err = xkcd.GetCurrent()
+	} else {
+		comic, err = xkcd.Get(*number)
+	}
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	e := xml.NewEncoder(os.Stdout)
+	err = e.Encode(comic)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
