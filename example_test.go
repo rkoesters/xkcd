bf4bd1ac329db7f186e327621be4a770608dd4e6
Ryan Koesters <rmkoesters@gmail.com>
2013-10-28 13:29:37 -0500

    Added example for `New'.

--- example_test.go
+++ example_test.go
@@ -4,7 +4,25 @@ import (
 	"fmt"
 	"github.com/rkoesters/xkcd"
 	"log"
+	"strings"
 )
+
+func ExampleNew() {
+	r := strings.NewReader(`{"month": "3", "num": 1190, "link": "http:\/\/geekwagon.net\/projects\/xkcd1190\/", "year": "2013", "news": "", "safe_title": "Time", "transcript": "", "alt": "The end.", "img": "http:\/\/imgs.xkcd.com\/comics\/time.png", "title": "Time", "day": "25"}`)
+
+	comic, err := xkcd.New(r)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Printf("Number: %v\n", comic.Num)
+	fmt.Printf("Image: %v\n", comic.Img)
+	fmt.Printf("Alt Text: %v\n", comic.Alt)
+	// Output:
+	// Number: 1190
+	// Image: http://imgs.xkcd.com/comics/time.png
+	// Alt Text: The end.
+}
 
 func ExampleGet() {
 	comic, err := xkcd.Get(140)
