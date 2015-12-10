1ac2c849528339e40e51e44f9f1d88bd2011abae
Ryan Koesters <rmkoesters@gmail.com>
2015-12-10 00:29:32 -0600

    Added popover for comic details.

--- xkcd-gtk/viewer.go
+++ xkcd-gtk/viewer.go
@@ -7,14 +7,22 @@ import (
 	"github.com/rkoesters/xkcd"
 	"log"
 	"math/rand"
+	"strconv"
+	"strings"
+	"time"
 )
 
 // Viewer is a struct holding a gtk window for viewing XKCD comics.
 type Viewer struct {
+	comic *xkcd.Comic
 	win   *gtk.Window
 	hdr   *gtk.HeaderBar
 	img   *gtk.Image
-	comic *xkcd.Comic
+	// The labels in the details popover.
+	detailsNumber *gtk.Label
+	detailsTitle  *gtk.Label
+	detailsDate   *gtk.Label
+	detailsLink   *gtk.Label
 }
 
 // New creates a new XKCD viewer window.
@@ -35,8 +43,7 @@ func New() (*Viewer, error) {
 	builder.ConnectSignals(map[string]interface{}{
 		"PreviousComic": v.PreviousComic,
 		"NextComic":     v.NextComic,
-		"RandomComic":   v.RandomComic,
-	})
+		"RandomComic":   v.RandomComic})
 
 	// We want access to Window, HeaderBar, and Image in the future,
 	// so lets get access to them now.
@@ -47,7 +54,7 @@ func New() (*Viewer, error) {
 	}
 	v.win, ok = obj.(*gtk.Window)
 	if !ok {
-		return nil, errors.New("window")
+		return nil, errors.New("error getting viewer-window")
 	}
 	obj, err = builder.GetObject("header")
 	if err != nil {
@@ -55,7 +62,7 @@ func New() (*Viewer, error) {
 	}
 	v.hdr, ok = obj.(*gtk.HeaderBar)
 	if !ok {
-		return nil, errors.New("headerbar")
+		return nil, errors.New("error getting header")
 	}
 	obj, err = builder.GetObject("comic-image")
 	if err != nil {
@@ -63,7 +70,25 @@ func New() (*Viewer, error) {
 	}
 	v.img, ok = obj.(*gtk.Image)
 	if !ok {
-		return nil, errors.New("image")
+		return nil, errors.New("error getting comic-image")
+	}
+
+	// Get details labels.
+	v.detailsNumber, err = getLabel(builder, "details-number")
+	if err != nil {
+		return nil, err
+	}
+	v.detailsTitle, err = getLabel(builder, "details-title")
+	if err != nil {
+		return nil, err
+	}
+	v.detailsDate, err = getLabel(builder, "details-date")
+	if err != nil {
+		return nil, err
+	}
+	v.detailsLink, err = getLabel(builder, "details-link")
+	if err != nil {
+		return nil, err
 	}
 
 	// Closing the window should exit the program.
@@ -119,5 +144,39 @@ func (v *Viewer) SetComic(n int) error {
 	v.img.SetFromFile(imgPath)
 	v.img.SetTooltipText(v.comic.Alt)
 
+	// Update the details popover.
+	v.detailsNumber.SetText(strconv.Itoa(v.comic.Num))
+	v.detailsTitle.SetText(v.comic.Title)
+	v.detailsDate.SetText(formatDate(v.comic.Year, v.comic.Month, v.comic.Day))
+	if v.comic.Link != "" {
+		fmtLink := fmt.Sprintf("<a href=\"%v\">%[1]v</a>", v.comic.Link)
+		v.detailsLink.SetMarkup(fmtLink)
+	} else {
+		v.detailsLink.SetText("")
+	}
+
 	return nil
+}
+
+// formatDate takes a year, month, and date as strings and turns them
+// into a pretty date.
+func formatDate(year, month, day string) string {
+	date := strings.Join([]string{year, month, day}, "-")
+	t, err := time.Parse("2006-1-2", date)
+	if err != nil {
+		return ""
+	}
+	return t.Format("Jan _2, 2006")
+}
+
+func getLabel(b *gtk.Builder, id string) (*gtk.Label, error) {
+	obj, err := b.GetObject(id)
+	if err != nil {
+		return nil, err
+	}
+	label, ok := obj.(*gtk.Label)
+	if !ok {
+		return nil, fmt.Errorf("error getting label: %v", id)
+	}
+	return label, nil
 }
